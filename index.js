d8c84f074b66b44e67b688d45a871e75f7289d6e
AlexKi07 <kimanialexk07@gmail.com>
2025-03-12 09:59:38 +0300

    Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2019", result: "W"},
+    { year: "2021", result: "L"},
+    { year: "2022", result: "L"},
+    { year: "2023", result: "L"},
+    { year: "2024", result: "L"},
+]
+
+function superbowlWin(record){
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+}
